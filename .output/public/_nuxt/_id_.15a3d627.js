b1ebebfe9d67bdcfee65f7a4c6b7c382386b512c
bourne <xuhui.yon@gmail.com>
2023-11-01 16:12:12 +0800

    rebuild

---
+++ .output/public/_nuxt/_id_.15a3d627.js
@@ -0,0 +1,1 @@
+import{s as e,q as t}from"./entry.8edfdea2.js";const o={__name:"[id]",setup(a){return(r,c)=>(t(),e("div",null,"嵌套路由detail动态路由"))}};export{o as default};
---
+++ .output/server/chunks/app/client.manifest.mjs
@@ -0,0 +1,293 @@
+const client_manifest = {
+  "_base.35430ddc.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "css": [
+      "base.e57c486d.css"
+    ],
+    "file": "base.35430ddc.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "base.e57c486d.css": {
+    "file": "base.e57c486d.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "_el-header.4b8c2ebf.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "css": [
+      "el-header.cc5448e8.css"
+    ],
+    "file": "el-header.4b8c2ebf.js",
+    "imports": [
+      "_base.35430ddc.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "el-header.cc5448e8.css": {
+    "file": "el-header.cc5448e8.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "_vue.f36acd1f.039a2e4c.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "vue.f36acd1f.039a2e4c.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "base.css": {
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true,
+    "file": "base.e57c486d.css",
+    "src": "base.css"
+  },
+  "components/AppHeader.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "AppHeader.57fbea1e.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "components/AppHeader.vue"
+  },
+  "components/AppSider.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "AppSider.2ccd9c13.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "components/AppSider.vue"
+  },
+  "el-header.css": {
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true,
+    "file": "el-header.cc5448e8.css",
+    "src": "el-header.css"
+  },
+  "i18n.config.ts?hash=bffaebcb&config=1": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "i18n.config.55c28c72.js",
+    "isDynamicEntry": true,
+    "src": "i18n.config.ts?hash=bffaebcb&config=1"
+  },
+  "layouts/custom.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "custom.3763fdf2.js",
+    "imports": [
+      "_el-header.4b8c2ebf.js",
+      "_base.35430ddc.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "layouts/custom.vue"
+  },
+  "layouts/default.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "default.b43b8472.js",
+    "imports": [
+      "components/AppHeader.vue",
+      "_el-header.4b8c2ebf.js",
+      "components/AppSider.vue",
+      "_base.35430ddc.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "layouts/default.vue"
+  },
+  "layouts/noSide.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "noSide.53efae7b.js",
+    "imports": [
+      "components/AppHeader.vue",
+      "_el-header.4b8c2ebf.js",
+      "_base.35430ddc.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "layouts/noSide.vue"
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-404.css": {
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true,
+    "file": "error-404.7fc72018.css",
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-404.css"
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "css": [],
+    "file": "error-404.99bf67a0.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_vue.f36acd1f.039a2e4c.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue"
+  },
+  "error-404.7fc72018.css": {
+    "file": "error-404.7fc72018.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-500.css": {
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true,
+    "file": "error-500.c5df6088.css",
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-500.css"
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "css": [],
+    "file": "error-500.a10e0e78.js",
+    "imports": [
+      "_vue.f36acd1f.039a2e4c.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+  },
+  "error-500.c5df6088.css": {
+    "file": "error-500.c5df6088.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "dynamicImports": [
+      "layouts/custom.vue",
+      "layouts/default.vue",
+      "layouts/noSide.vue",
+      "i18n.config.ts?hash=bffaebcb&config=1",
+      "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue",
+      "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+    ],
+    "file": "entry.8edfdea2.js",
+    "isEntry": true,
+    "src": "node_modules/nuxt/dist/app/entry.js",
+    "_globalCSS": true
+  },
+  "pages/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "index.fb90b419.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/index.vue"
+  },
+  "pages/login/index.css": {
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true,
+    "file": "index.b85cdcc7.css",
+    "src": "pages/login/index.css"
+  },
+  "pages/login/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "css": [],
+    "file": "index.ce65bd5f.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_base.35430ddc.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/login/index.vue"
+  },
+  "index.b85cdcc7.css": {
+    "file": "index.b85cdcc7.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "pages/nested/detail/[id].vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "_id_.15a3d627.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/nested/detail/[id].vue"
+  },
+  "pages/nested/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "index.b5a09d29.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/nested/index.vue"
+  },
+  "pages/single/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "index.c8fc44db.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/single/index.vue"
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
