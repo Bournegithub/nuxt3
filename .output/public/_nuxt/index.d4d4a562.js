0b4fafca6b14bb9007be1123fcdfe76402b8f8c6
bourne <xuhui.yon@gmail.com>
2023-11-01 16:10:14 +0800

    rebuild

--- .output/public/_nuxt/index.d4d4a562.js
+++
@@ -1,1 +0,0 @@
-import{s as e,q as n}from"./entry.778072cb.js";const c={__name:"index",setup(t){return(a,r)=>(n(),e("div",null,"single-example"))}};export{c as default};
--- .output/public/_nuxt/index.dd621ab9.js
+++
@@ -1,1 +0,0 @@
-import{s as e,q as t}from"./entry.778072cb.js";const c={__name:"index",setup(n){return(r,s)=>(t(),e("div",null,"嵌套路由list"))}};export{c as default};
